a563d128ab7c9a74f22fe9277580534c32859790
lexqh <127494227+lexqhh@users.noreply.github.com>
2025-03-15 18:54:44 +0100

    Add files via upload

--- app/data_manager.py
+++ app/data_manager.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DB_USER = os.getenv("user")
 DB_PASSWORD = os.getenv("password")
 DB_HOST = os.getenv("host")
-DB_PORT = os.getenv("port")
+DB_PORT = os.getenv("port", "5432")  # Valeur par défaut 5432
 DB_NAME = os.getenv("dbname")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
